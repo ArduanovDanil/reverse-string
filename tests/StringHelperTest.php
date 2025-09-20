50ca287fc200e0ad06a47074be25aa7ea2b5e119
Arduanov Danil <rylezz@gmail.com>
2025-09-20 11:37:48 +0500

    Add test for numbers

--- tests/StringHelperTest.php
+++ tests/StringHelperTest.php
@@ -21,13 +21,13 @@ class StringHelperTest extends TestCase
         $this->assertEquals('olleh', $reversed);
     }
 
-    public function testReverseStringWithUpperCase()
+    public function testReverseStringUpperCase()
     {
         $reversed = $this->stringHelper->reverseString('Hello World');
         $this->assertEquals('Olleh Dlrow', $reversed);
     }
 
-    public function testReverseStringWithSeparators()
+    public function testReverseStringSeparators()
     {
         $reversed = $this->stringHelper->reverseString('Hello big-World with all-sep`aratOrs');
         $this->assertEquals('Olleh gib-Dlrow htiw lla-pes`srotAra', $reversed);
@@ -39,12 +39,17 @@ class StringHelperTest extends TestCase
         $this->assertEquals('Olleh drow_ni_erocsrednu', $reversed);
     }
 
-    public function testReverseStringWitDots()
+    public function testReverseStringDots()
     {
         $reversed = $this->stringHelper->reverseString('Hello String.with.dots');
         $this->assertEquals('Olleh Stod.htiw.gnirts', $reversed);
     }
 
+    public function testReverseStringNumbers()
+    {
+        $reversed = $this->stringHelper->reverseString('Hello! 100 students at 123th Avenue');
+        $this->assertEquals('Olleh! 001 stneduts ta ht321 Euneva', $reversed);
+    }
 
     public function testReverseStringPunctuationMarks()
     {
